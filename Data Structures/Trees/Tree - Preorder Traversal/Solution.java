fc8dbfec56c79864be6188283ff52af549224cc3
EDWIN\Owner <gohbiscuit@gmail.com>
2017-03-24 19:06:52 +0800

    - updated solution files

--- Data Structures/Trees/Tree - Preorder Traversal/Solution.java
+++ Data Structures/Trees/Tree - Preorder Traversal/Solution.java
@@ -1,4 +1,21 @@
+/* you only have to complete the function given below.  
+Node is defined as  
 
-public class Solution {
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
 
+*/
+
+/* Recursive Solution */
+void preOrder(Node root) {
+  
+  if(root == null) return;
+  
+  System.out.print(root.data + " ");
+  
+  preOrder( root.left );
+  preOrder( root.right ); 
 }
